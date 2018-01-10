b60641b5ad109c712104ddf012967a652768eca4
liuyk <271007729@qq.com>
2018-01-11 01:22:08 +0800

    添加样式

--- src/main/java/com/sysware/cloud/web/view/AthleteViewController.java
+++ src/main/java/com/sysware/cloud/web/view/AthleteViewController.java
@@ -1,12 +1,7 @@
 package com.sysware.cloud.web.view;
 
-import com.sysware.cloud.dto.AthleteDto;
-import com.sysware.cloud.entity.Athlete;
-import com.sysware.cloud.service.AthleteService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AthleteViewController {
